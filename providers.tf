cd8c288b836191da7371f2f4e4b0275d102a8df1
ssafarveisi-dataops <sajad.safarveisi@cognism.com>
2026-01-29 00:01:57 +0100

    Removed profile from providers

--- providers.tf
+++ providers.tf
@@ -19,12 +19,9 @@ terraform {
     encrypt      = true
     kms_key_id   = "arn:aws:kms:eu-central-1:598520881431:key/c028cd8f-4a67-42ae-9054-bdf6c7999fde"
     key          = "terraform/aws/state-files/example2/terraform.tfstate"
-    profile      = "default"
   }
 }
 
 provider "aws" {
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "default"
-  region                   = "eu-central-1"
+  region = "eu-central-1"
 }
